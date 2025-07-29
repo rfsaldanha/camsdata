6cf441fee193cac9ae5fd6a36022538356b08079
rfsaldanha <rfsaldanha@gmail.com>
2025-07-29 15:45:44 -0300

    Update

--- cams_forecast.R
+++ cams_forecast.R
@@ -23,15 +23,16 @@ library(duckdb)
 bbox <- c(33, -118, -56, -30)
 
 # Download directory
-dir_data <- "/dados/home/rfsaldanha/camsdata/forecast_data/"
-# dir_data <- "forecast_data/"
+# dir_data <- "/dados/home/rfsaldanha/camsdata/forecast_data/"
+dir_data <- "forecast_data/"
 
 # Forecast range, in hours
 leadtime_hour <- as.character(0:120)
+leadtime_hour_level <- as.character(seq(0, 120, 3))
 
 # Set update reference time
 if (
-  now(tzone = "UTC") < as_datetime(today(tzone = "UTC") + duration("14 hours"))
+  now(tzone = "UTC") < as_datetime(today(tzone = "UTC") + duration("18 hours"))
 ) {
   date <- today()
   time <- "00:00"
@@ -49,6 +50,15 @@ file_name_pm25 <- glue(
 file_name_o3 <- glue(
   "cams_forecast_o3.nc"
 )
+file_name_co <- glue(
+  "cams_forecast_co.nc"
+)
+file_name_no2 <- glue(
+  "cams_forecast_no2.nc"
+)
+file_name_so2 <- glue(
+  "cams_forecast_so2.nc"
+)
 file_name_temp <- glue(
   "cams_forecast_temp.nc"
 )
@@ -64,7 +74,7 @@ cli_alert_info("Reading geometries file...")
 # mun <- geobr::read_municipality(year = 2010, simplified = TRUE)
 # mun <- st_transform(x = mun, crs = 4326)
 # saveRDS(mun, "mun_epsg4326.rds")
-mun <- readRDS("/dados/home/rfsaldanha/camsdata/mun_epsg4326.rds")
+mun <- readRDS(path(dir_data, "mun_epsg4326.rds"))
 
 # Declare requests
 ## PM2.5
@@ -84,15 +94,61 @@ request_pm25 <- list(
 ## O3
 request_o3 <- list(
   dataset_short_name = "cams-global-atmospheric-composition-forecasts",
-  variable = "total_column_ozone",
+  variable = "ozone",
+  pressure_level = "1000",
   date = glue("{date}/{date}"),
   time = time,
-  leadtime_hour = leadtime_hour,
+  leadtime_hour = leadtime_hour_level,
   type = "forecast",
   data_format = "netcdf",
   download_format = "unarchived",
   area = bbox,
   target = file_name_o3
+)
+
+## CO
+request_co <- list(
+  dataset_short_name = "cams-global-atmospheric-composition-forecasts",
+  variable = "carbon_monoxide",
+  pressure_level = "1000",
+  date = glue("{date}/{date}"),
+  time = time,
+  leadtime_hour = leadtime_hour_level,
+  type = "forecast",
+  data_format = "netcdf",
+  download_format = "unarchived",
+  area = bbox,
+  target = file_name_co
+)
+
+## NO2
+request_no2 <- list(
+  dataset_short_name = "cams-global-atmospheric-composition-forecasts",
+  variable = "nitrogen_dioxide",
+  pressure_level = "1000",
+  date = glue("{date}/{date}"),
+  time = time,
+  leadtime_hour = leadtime_hour_level,
+  type = "forecast",
+  data_format = "netcdf",
+  download_format = "unarchived",
+  area = bbox,
+  target = file_name_no2
+)
+
+## SO2
+request_so2 <- list(
+  dataset_short_name = "cams-global-atmospheric-composition-forecasts",
+  variable = "sulphur_dioxide",
+  pressure_level = "1000",
+  date = glue("{date}/{date}"),
+  time = time,
+  leadtime_hour = leadtime_hour_level,
+  type = "forecast",
+  data_format = "netcdf",
+  download_format = "unarchived",
+  area = bbox,
+  target = file_name_so2
 )
 
 ## Temperature
@@ -158,6 +214,48 @@ retry(
   until = ~ is_file(as.character(.))
 )
 
+cli_h3("CO")
+retry(
+  expr = {
+    wf_request(
+      request = request_co,
+      transfer = TRUE,
+      path = dir_data
+    )
+  },
+  interval = 1,
+  max_tries = 100,
+  until = ~ is_file(as.character(.))
+)
+
+cli_h3("NO2")
+retry(
+  expr = {
+    wf_request(
+      request = request_no2,
+      transfer = TRUE,
+      path = dir_data
+    )
+  },
+  interval = 1,
+  max_tries = 100,
+  until = ~ is_file(as.character(.))
+)
+
+cli_h3("SO2")
+retry(
+  expr = {
+    wf_request(
+      request = request_so2,
+      transfer = TRUE,
+      path = dir_data
+    )
+  },
+  interval = 1,
+  max_tries = 100,
+  until = ~ is_file(as.character(.))
+)
+
 cli_h3("Temperature")
 retry(
   expr = {
@@ -197,6 +295,9 @@ cli_alert_info("Connecting to database...")
 con <- dbConnect(duckdb(), path(dir_data, "cams_forecast.duckdb"))
 tb_name_pm25 <- "pm25_mun_forecast"
 tb_name_o3 <- "o3_mun_forecast"
+tb_name_co <- "co_mun_forecast"
+tb_name_no2 <- "no2_mun_forecast"
+tb_name_so2 <- "so2_mun_forecast"
 tb_name_temp <- "temp_mun_forecast"
 tb_name_uv <- "uv_mun_forecast"
 
@@ -213,18 +314,20 @@ agg_pm25 <- function(rst, x, fun) {
   # Zonal statistic computation
   tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
 
-  # Table output with unit conversion and rounding
-  sel_time <- as.numeric(str_sub(string = time, start = 1, end = 2))
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "1 hours"
+  )
 
+  # Table output with unit conversion and rounding
   res <- tibble(
     code_muni = mun$code_muni,
-    date = date,
+    date = seq_dates[x],
     value = round(x = tmp * 1000000000, digits = 2), # kg/m3 to μg/m3
   ) |>
     mutate(
-      # The date and time of the forecast is the model run time (sel_time) plus the forecast depth.
-      # Depth = 1 equivales to hour 0, depth = 121 equivales to hour 120
-      date = date + duration(sel_time + (x - 1), "hour"),
       date = with_tz(date, "America/Sao_Paulo")
     )
 
@@ -263,18 +366,21 @@ agg_o3 <- function(rst, x, fun) {
   # Zonal statistic computation
   tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
 
-  # Table output with unit conversion and rounding
-  sel_time <- as.numeric(str_sub(string = time, start = 1, end = 2))
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "3 hours"
+  )
 
+  # Table output with unit conversion and rounding
   res <- tibble(
     code_muni = mun$code_muni,
-    date = date,
-    value = round(x = tmp * 44698, digits = 2), # kg/m2 to DU
+    date = seq_dates[x],
+    value = round(x = tmp * 28.9644 / 47.9982 * 1e9, digits = 2), # kg/kg to μg/m3
+    # https://forum.ecmwf.int/t/convert-mass-mixing-ratio-mmr-to-mass-concentration-or-to-volume-mixing-ratio-vmr/1253/2
   ) |>
     mutate(
-      # The date and time of the forecast is the model run time (sel_time) plus the forecast depth.
-      # Depth = 1 equivales to hour 0, depth = 121 equivales to hour 120
-      date = date + duration(sel_time + (x - 1), "hour"),
       date = with_tz(date, "America/Sao_Paulo")
     )
 
@@ -287,7 +393,7 @@ agg_o3 <- function(rst, x, fun) {
 # Compute zonal mean
 cli_alert_info("Computing zonal mean...")
 res_mean_o3 <- map(
-  .x = 1:121,
+  .x = 1:41,
   .f = agg_o3,
   rst = rst_o3,
   fun = "mean",
@@ -299,6 +405,169 @@ cli_alert_success("Done!")
 cli_alert_info("Checking data...")
 tbl(con, tb_name_o3) |> tally()
 tbl(con, tb_name_o3) |> head()
+
+
+cli_h3("CO")
+
+# Read CAMS file
+cli_alert_info("Reading forecast file...")
+rst_co <- terra::rast(path(dir_data, file_name_co))
+cli_alert_info("Projecting raster file...")
+rst_co <- project(x = rst_co, "EPSG:4326")
+
+# Zonal statistic function
+agg_co <- function(rst, x, fun) {
+  # Zonal statistic computation
+  tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
+
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "3 hours"
+  )
+
+  # Table output with unit conversion and rounding
+  res <- tibble(
+    code_muni = mun$code_muni,
+    date = seq_dates[x],
+    value = round(x = tmp * 28.9644 / 28.0101 * 1e9, digits = 2), # kg/kg to μg/m3
+    # https://forum.ecmwf.int/t/convert-mass-mixing-ratio-mmr-to-mass-concentration-or-to-volume-mixing-ratio-vmr/1253/2
+  ) |>
+    mutate(
+      date = with_tz(date, "America/Sao_Paulo")
+    )
+
+  # Write to database
+  dbWriteTable(conn = con, name = tb_name_co, value = res, append = TRUE)
+
+  return(TRUE)
+}
+
+# Compute zonal mean
+cli_alert_info("Computing zonal mean...")
+res_mean_co <- map(
+  .x = 1:41,
+  .f = agg_co,
+  rst = rst_co,
+  fun = "mean",
+  .progress = TRUE
+)
+cli_alert_success("Done!")
+
+# Check data
+cli_alert_info("Checking data...")
+tbl(con, tb_name_co) |> tally()
+tbl(con, tb_name_co) |> head()
+
+
+cli_h3("NO2")
+
+# Read CAMS file
+cli_alert_info("Reading forecast file...")
+rst_no2 <- terra::rast(path(dir_data, file_name_co))
+cli_alert_info("Projecting raster file...")
+rst_no2 <- project(x = rst_no2, "EPSG:4326")
+
+# Zonal statistic function
+agg_no2 <- function(rst, x, fun) {
+  # Zonal statistic computation
+  tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
+
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "3 hours"
+  )
+
+  # Table output with unit conversion and rounding
+  res <- tibble(
+    code_muni = mun$code_muni,
+    date = seq_dates[x],
+    value = round(x = tmp * 28.9644 / 46.0055 * 1e9, digits = 2), # kg/kg to μg/m3
+    # https://forum.ecmwf.int/t/convert-mass-mixing-ratio-mmr-to-mass-concentration-or-to-volume-mixing-ratio-vmr/1253/2
+  ) |>
+    mutate(
+      date = with_tz(date, "America/Sao_Paulo")
+    )
+
+  # Write to database
+  dbWriteTable(conn = con, name = tb_name_no2, value = res, append = TRUE)
+
+  return(TRUE)
+}
+
+# Compute zonal mean
+cli_alert_info("Computing zonal mean...")
+res_mean_no2 <- map(
+  .x = 1:41,
+  .f = agg_no2,
+  rst = rst_no2,
+  fun = "mean",
+  .progress = TRUE
+)
+cli_alert_success("Done!")
+
+# Check data
+cli_alert_info("Checking data...")
+tbl(con, tb_name_no2) |> tally()
+tbl(con, tb_name_no2) |> head()
+
+
+cli_h3("SO2")
+
+# Read CAMS file
+cli_alert_info("Reading forecast file...")
+rst_so2 <- terra::rast(path(dir_data, file_name_co))
+cli_alert_info("Projecting raster file...")
+rst_so2 <- project(x = rst_so2, "EPSG:4326")
+
+# Zonal statistic function
+agg_so2 <- function(rst, x, fun) {
+  # Zonal statistic computation
+  tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
+
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "3 hours"
+  )
+
+  # Table output with unit conversion and rounding
+  res <- tibble(
+    code_muni = mun$code_muni,
+    date = seq_dates[x],
+    value = round(x = tmp * 28.9644 / 64.0638 * 1e9, digits = 2), # kg/kg to μg/m3
+    # https://forum.ecmwf.int/t/convert-mass-mixing-ratio-mmr-to-mass-concentration-or-to-volume-mixing-ratio-vmr/1253/2
+  ) |>
+    mutate(
+      date = with_tz(date, "America/Sao_Paulo")
+    )
+
+  # Write to database
+  dbWriteTable(conn = con, name = tb_name_so2, value = res, append = TRUE)
+
+  return(TRUE)
+}
+
+# Compute zonal mean
+cli_alert_info("Computing zonal mean...")
+res_mean_so2 <- map(
+  .x = 1:41,
+  .f = agg_so2,
+  rst = rst_so2,
+  fun = "mean",
+  .progress = TRUE
+)
+cli_alert_success("Done!")
+
+# Check data
+cli_alert_info("Checking data...")
+tbl(con, tb_name_so2) |> tally()
+tbl(con, tb_name_so2) |> head()
+
 
 cli_h3("Temperature")
 
@@ -313,18 +582,20 @@ agg_temp <- function(rst, x, fun) {
   # Zonal statistic computation
   tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
 
-  # Table output with unit conversion and rounding
-  sel_time <- as.numeric(str_sub(string = time, start = 1, end = 2))
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "1 hours"
+  )
 
+  # Table output with unit conversion and rounding
   res <- tibble(
     code_muni = mun$code_muni,
-    date = date,
+    date = seq_dates[x],
     value = round(x = tmp - 272.15, digits = 2), # K to °C
   ) |>
     mutate(
-      # The date and time of the forecast is the model run time (sel_time) plus the forecast depth.
-      # Depth = 1 equivales to hour 0, depth = 121 equivales to hour 120
-      date = date + duration(sel_time + (x - 1), "hour"),
       date = with_tz(date, "America/Sao_Paulo")
     )
 
@@ -363,18 +634,20 @@ agg_uv <- function(rst, x, fun) {
   # Zonal statistic computation
   tmp <- exact_extract(x = rst[[x]], y = mun, fun = fun, progress = FALSE)
 
-  # Table output with unit conversion and rounding
-  sel_time <- as.numeric(str_sub(string = time, start = 1, end = 2))
+  # Date and time
+  seq_dates <- seq(
+    from = as_datetime(paste(date, time), format = "%Y-%m-%d %H:%M"),
+    to = as_datetime(date + duration(120, "hours")),
+    by = "1 hours"
+  )
 
+  # Table output with unit conversion and rounding
   res <- tibble(
     code_muni = mun$code_muni,
-    date = date,
+    date = seq_dates[x],
     value = round(x = tmp * 40, digits = 2), # Wm2 to UVI
   ) |>
     mutate(
-      # The date and time of the forecast is the model run time (sel_time) plus the forecast depth.
-      # Depth = 1 equivales to hour 0, depth = 121 equivales to hour 120
-      date = date + duration(sel_time + (x - 1), "hour"),
       date = with_tz(date, "America/Sao_Paulo")
     )
 
@@ -400,7 +673,11 @@ cli_alert_info("Checking data...")
 tbl(con, tb_name_uv) |> tally()
 tbl(con, tb_name_uv) |> head()
 
-# Fetch bdqueimadas data
+# Database disconnect
+cli_alert_info("Disconnecting database...")
+dbDisconnect(conn = con)
+
+# Fetch INPE BD Queimadas data
 bdq_base_url <- "https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/diario/America_Sul/"
 bdq_file_names <- paste0(
   "focos_diario_",
@@ -427,9 +704,5 @@ for (i in bdq_urls) {
   rm(tmp)
 }
 saveRDS(object = bdq_focos, file = path(dir_data, "bdq_focos.rds"))
-
-# Database disconnect
-cli_alert_info("Disconnecting database...")
-dbDisconnect(conn = con)
 
 cli_h1("END")
