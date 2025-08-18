2b93042bb8c8bc878d30ae662ba0743eb97fff56
Raphael Saldanha <rfsaldanha@gmail.com>
2025-08-18 11:59:06 -0300

    Biomass burning (organic matter) aerosol

--- teste_aerosol.R
+++ teste_aerosol.R
@@ -13,7 +13,7 @@ bbox <- c(13.49, -83.15, -56.69, -32.20)
 
 request_aerosol <- list(
   dataset_short_name = "cams-global-atmospheric-composition-forecasts",
-  variable = "total_aerosol_optical_depth_550nm",
+  variable = "organic_matter_aerosol_optical_depth_550nm",
   date = glue("{date}/{date}"),
   time = time,
   leadtime_hour = leadtime_hour,
