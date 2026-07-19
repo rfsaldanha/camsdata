7954580ddfd4abd5fd68a9bc8c695cb57b6bbab2
Raphael Saldanha <rfsaldanha@gmail.com>
2026-07-19 20:58:29 -0300

    Atualiza datas

--- cams_temp_download.R
+++ cams_temp_download.R
@@ -13,7 +13,7 @@ wf_set_key(key = Sys.getenv("era5_API_Key"))
 # dir_data <- "/media/raphaelsaldanha/lacie/cams_temp"
 # dir_data <- "/dados2/data/cams/cams_temp"
 dir_data <- "~/Downloads/cams/cams_temp"
-dates <- as.character(seq(ymd("2025-01-01"), ymd("2025-08-31"), by = "1 day"))
+dates <- as.character(seq(ymd("2025-01-01"), ymd("2025-12-31"), by = "1 day"))
 dates <- rev(dates)
 times <- c(
   "00:00",
